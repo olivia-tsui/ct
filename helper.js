ab01121e2960f2728aa036426b8c400ac0cfce43
Marco Christian Krenn <marco.krenn@gmail.com>
2023-03-23 15:06:03 +0100

    update helper.js to output light and dark scales

--- helper.js
+++ helper.js
@@ -1,312 +1,197 @@
 import chroma from "chroma-js";
-export function download(data,name) {
 
-    const blob = new Blob([JSON.stringify(data)], { type: "application/json" })
-    const url = URL.createObjectURL(blob)
-    const link = document.createElement('a')
-    link.setAttribute('href', url)
-    link.setAttribute('download', `${name}.json`)
-    link.click()
+export function download(data, name) {
+  const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", `${name}.json`);
+  link.click();
 }
+
 export function reformat(configs) {
-  let outputs = []
-  configs.forEach((config) => {
-    outputs.push(reformatOne(config))
+  let outputs = configs.map(generateColorScale);
+
+  let scale = {};
+  outputs.forEach((n, i) => {
+    return (scale[toKebabCase(configs[i]["name"])] = n);
   });
-  let scale = {}
-   outputs.forEach((n,i)=>{
-    
-    return scale[toKebabCase(configs[i]['name'])]=n
-  })
-  // scale.json
-  download(  {
-    brand: {
-      color: {
-       scale
+
+  download(
+    {
+      brand: {
+        color: {
+          scale,
+        },
       },
     },
-  },"scale")
+    "scale"
+  );
 
+  let prefix = "brand.color.scale";
+  let stepsLight = {
+    weak: ["l2", "l4", "l6", "l8", "l10", "l12"],
+    strong: ["d2", "d4", "d6", "d8", "d10", "d12"],
+    light: ["l2", "l4", "l6", "l8", "l10", "l12"],
+    dark: ["d2", "d4", "d6", "d8", "d10", "d12"],
+    base: "base"
+  };
+  let stepsDark = {
+    weak: ["d1", "d3", "d5", "d7", "d9", "d12"],
+    strong: ["l3", "l5", "l7", "l9", "l11", "l12"],
+    light: ["l3", "l5", "l7", "l9", "l11", "l12"],
+    dark: ["d1", "d3", "d5", "d7", "d9", "d12"],
+    base: "l1"
+  };
+  let stepsDim = {
+    weak: ["d3", "d5", "d7", "d9", "d11", "d12"],
+    strong: ["l1", "l3", "l5", "l7", "l9", "l12"],
+    light: ["l1", "l3", "l5", "l7", "l9", "l12"],
+    dark: ["d3", "d5", "d7", "d9", "d11", "d12"],
+    base: "d1"
+  };
   
-  // light.json
-  let lightColor = {}
-  let light = {
-    brand:{
-      color:lightColor
-    }
-  }
-  configs.forEach((config) => {
-    let scaleName = toKebabCase( config.name)
-    lightColor[scaleName]={
-      "weak-6": {
-        value: `{brand.color.scale.${scaleName}.l12}`,
-        type: "color",
-      },
-      "weak-5": {
-        value: `{brand.color.scale.${scaleName}.l10}`,
-        type: "color",
-      },
-      "weak-4": {
-        value: `{brand.color.scale.${scaleName}.l08}`,
-        type: "color",
-      },
-      "weak-3": {
-        value: `{brand.color.scale.${scaleName}.l06}`,
-        type: "color",
-      },
-      "weak-2": {
-        value: `{brand.color.scale.${scaleName}.l04}`,
-        type: "color",
-      },
-      "weak-1": {
-        value: `{brand.color.scale.${scaleName}.l02}`,
-        type: "color",
-      },
-      base: {
-        value: `{brand.color.scale.${scaleName}.base}`,
-        type: "color",
-      },
-      "strong-1": {
-        value: `{brand.color.scale.${scaleName}.d02}`,
-        type: "color",
-      },
-      "strong-2": {
-        value: `{brand.color.scale.${scaleName}.d04}`,
-        type: "color",
-      },
-      "strong-3": {
-        value: `{brand.color.scale.${scaleName}.d06}`,
-        type: "color",
-      },
-      "strong-4": {
-        value: `{brand.color.scale.${scaleName}.d08}`,
-        type: "color",
-      },
-      "strong-5": {
-        value: `{brand.color.scale.${scaleName}.d10}`,
-        type: "color",
-      },
-      "strong-6": {
-        value: `{brand.color.scale.${scaleName}.d12}`,
-        type: "color",
-      },
-    }
-  })
-  download(light,"light")
- 
-  // dark.json
-  let darkColor={}
-  let dark = {
-    brand:{
-      color:darkColor
-    }
-  }
-  configs.forEach((config) => {
-    let scaleName = toKebabCase( config.name)
-    darkColor[scaleName]={"strong-6": {
-    "value": `{brand.color.scale.${scaleName}.l12}`,
-    "type": "color",
-  },
-  "strong-5": {
-    "value": `{brand.color.scale.${scaleName}.l11}`,
-    "type": "color",
-  },
-  "strong-4": {
-    "value": `{brand.color.scale.${scaleName}.l09}`,
-    "type": "color",
-  },
-  "strong-3": {
-    "value": `{brand.color.scale.${scaleName}.l07}`,
-    "type": "color",
-  },
-  "strong-2": {
-    "value": `{brand.color.scale.${scaleName}.l05}`,
-    "type": "color",
-  },
-  "strong-1": {
-    "value": `{brand.color.scale.${scaleName}.l03}`,
-    "type": "color",
-  },
-  "base": {
-    "value": `{brand.color.scale.${scaleName}.l01}`,
-    "type": "color",
-  },
-  "weak-1": {
-    "value": `{brand.color.scale.${scaleName}.d01}`,
-    "type": "color",
-  },
-  "weak-2": {
-    "value": `{brand.color.scale.${scaleName}.d03}`,
-    "type": "color",
-  },
-  "weak-3": {
-    "value": `{brand.color.scale.${scaleName}.d05}`,
-    "type": "color",
-  },
-  "weak-4": {
-    "value": `{brand.color.scale.${scaleName}.d07}`,
-    "type": "color",
-  },
-  "weak-5": {
-    "value": `{brand.color.scale.${scaleName}.d09}`,
-    "type": "color",
-  },
-  "weak-6": {
-    "value": `{brand.color.scale.${scaleName}.d12}`,
-    "type": "color",
-  }}
-})
-  download(dark,"dark")
+  let light = createColorObject(configs, stepsLight, prefix);
+  let dark = createColorObject(configs, stepsDark, prefix);
+  let dim = createColorObject(configs, stepsDim, prefix);
 
-  // dim.json
-  let dimColor={}
-  let dim = {
+
+  download(light, "light");
+  download(dark, "dark");
+  download(dim, "dim");
+}
+
+function createColorObject(configs, steps, prefix) {
+  let colorObj = {};
+
+  configs.forEach((config) => {
+    let scaleName = toKebabCase(config.name);
+    colorObj[scaleName] = {};
+
+    // Add weak colors
+    for (let index = steps['weak'].length - 1; index >= 0; index--) {
+      const step = steps['weak'][index];
+      colorObj[scaleName][`weak-${index + 1}`] = {
+        value: `{${prefix}.${scaleName}.${step}}`,
+        type: "color",
+      };
+    }
+
+    // Add base color
+    colorObj[scaleName]["base"] = {
+      value: `{${prefix}.${scaleName}.${steps['base']}}`,
+      type: "color",
+    };
+
+    // Add strong colors
+    steps['strong'].forEach((step, index) => {
+      colorObj[scaleName][`strong-${index + 1}`] = {
+        value: `{${prefix}.${scaleName}.${step}}`,
+        type: "color",
+      };
+    });
+
+    // Add light colors
+    for (let index = steps['light'].length - 1; index >= 0; index--) {
+      const step = steps['light'][index];
+      colorObj[scaleName][`light-${index + 1}`] = {
+        value: `{${prefix}.${scaleName}.${step}}`,
+        type: "color",
+      };
+    }
+
+    // Add base color
+    colorObj[scaleName]["_base"] = {
+      value: `{${prefix}.${scaleName}.${steps['base']}}`,
+      type: "color",
+    };
+
+    // Add dark colors
+    steps['dark'].forEach((step, index) => {
+      colorObj[scaleName][`dark-${index + 1}`] = {
+        value: `{${prefix}.${scaleName}.${step}}`,
+        type: "color",
+      };
+    });
+
+  });
+  
+
+  return {
     brand: {
-      color: dimColor,
+      color: colorObj,
     },
   };
-  configs.forEach((config) => {
-    let scaleName = toKebabCase( config.name)
-    dimColor[scaleName]={
-      "strong-6": {
-        "value": `{brand.color.scale.${scaleName}.l12}`,
-        "type": "color",
-      },
-      "strong-5": {
-        "value": `{brand.color.scale.${scaleName}.l09}`,
-        "type": "color",
-      },
-      "strong-4": {
-        "value": `{brand.color.scale.${scaleName}.l07}`,
-        "type": "color",
-      },
-      "strong-3": {
-        "value": `{brand.color.scale.${scaleName}.l05}`,
-        "type": "color",
-      },
-      "strong-2": {
-        "value": `{brand.color.scale.${scaleName}.l03}`,
-        "type": "color",
-      },
-      "strong-1": {
-        "value": `{brand.color.scale.${scaleName}.l01}`,
-        "type": "color",
-      },
-      "base": {
-        "value": `{brand.color.scale.${scaleName}.d01}`,
-        "type": "color",
-      },
-      "weak-1": {
-        "value": `{brand.color.scale.${scaleName}.d03}`,
-        "type": "color",
-      },
-      "weak-2": {
-        "value": `{brand.color.scale.${scaleName}.d05}`,
-        "type": "color",
-      },
-      "weak-3": {
-        "value": `{brand.color.scale.${scaleName}.d07}`,
-        "type": "color",
-      },
-      "weak-4": {
-        "value": `{brand.color.scale.${scaleName}.d09}`,
-        "type": "color",
-      },
-      "weak-5": {
-        "value": `{brand.color.scale.${scaleName}.d11}`,
-        "type": "color",
-      },
-      "weak-6": {
-        "value": `{brand.color.scale.${scaleName}.d12}`,
-        "type": "color",
-      }
-    }
-  })
-  download(dim,"dim")
-
 }
+
 
 function toKebabCase(str) {
   return str
-  .replace(/([a-z])([A-Z])/g, "$1-$2")
-  .replace(/[\s_]+/g, '-')
-  .toLowerCase();
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/[\s_]+/g, "-")
+    .toLowerCase();
 }
- function reformatOne(config) {
-   let shades = [];
-   let names = [];
-   let light = chroma(config.baseValue)
-     .set("hsl.h", config.lightHueShift)
-     .luminance(config.lightLuminance);
-   let dark = chroma(config.baseValue)
-     .set("hsl.h", config.darkHueShift)
-     .luminance(config.darkLuminance);
-   if (config.saturation > 0) {
-     light = light.saturate(config.saturation);
-   } else if (config.saturation < 0) {
-     light = light.desaturate(-config.saturation);
-   }
-   if (config.darkSaturation > 0) {
-     dark = dark.saturate(config.darkSaturation);
-   } else if (config.darkSaturation < 0) {
-     dark = dark.desaturate(-config.darkSaturation);
-   }
 
-   let lightScale = chroma
-     .scale([config.baseValue, light.hex()])
-     .mode("hsl")
-     .domain(config.lightDomain)
-     .colors(config.stepsLighter + 1);
-   let darkScale = chroma
-     .scale([config.baseValue, dark.hex()])
-     .mode("hsl")
-     .domain(config.darkDomain)
-     .colors(config.stepsDarker + 1);
+function generateColorScale(config) {
+  let light = chroma(config.baseValue)
+    .set("hsl.h", config.lightHueShift)
+    .luminance(config.lightLuminance)
+    .saturate(config.saturation > 0 ? config.saturation : -config.saturation);
 
-   for (let i = lightScale.length - 1; i > 0; i--) {
-     let newShade = lightScale[i];
-     shades.push(newShade);
-     names.push(`l${i}`);
-   }
-   // add base
-   shades.push(config.baseValue);
-   names.push("base");
+  let dark = chroma(config.baseValue)
+    .set("hsl.h", config.darkHueShift)
+    .luminance(config.darkLuminance)
+    .saturate(config.darkSaturation > 0 ? config.darkSaturation : -config.darkSaturation);
 
-   for (let i = 1; i < darkScale.length; i++) {
-     let newShade = darkScale[i];
-     shades.push(newShade);
-     names.push(`d${i}`);
-   }
-   let fixedHues = shades.map((color) => {
-     return chroma(color).get("hsl.h").toFixed(2);
-   });
+  let lightScale = chroma
+    .scale([config.baseValue, light.hex()])
+    .mode("hsl")
+    .domain(config.lightDomain)
+    .colors(config.stepsLighter + 1);
 
-   // updated colors
-   const updatedColors = shades;
+  let darkScale = chroma
+    .scale([config.baseValue, dark.hex()])
+    .mode("hsl")
+    .domain(config.darkDomain)
+    .colors(config.stepsDarker + 1);
 
-   if (config.manualAdjustments && config.manualAdjustments.lightness) {
-     Object.keys(config.manualAdjustments.lightness).forEach((key) => {
-       updatedColors[+key] = chroma(updatedColors[+key])
-         .set("hsl.h", fixedHues[+key])
-         .set("hsl.l", config.manualAdjustments.lightness[+key])
-         .hex();
-     });
-   }
-   if (config.manualAdjustments && config.manualAdjustments.saturation) {
-     Object.keys(config.manualAdjustments.saturation).forEach((key) => {
-       updatedColors[+key] = chroma(updatedColors[+key])
-         .set("hsl.h", fixedHues[+key])
-         .set("hsl.s", config.manualAdjustments.saturation[+key])
-         .hex();
-     });
-   }
-   let output = {};
-   names = names.reverse()
-   updatedColors.reverse().forEach((color, index) => {
-     output[ names[index]] = {
-       value: color,
-       type: "color",
-     };
-   });
-   return output;
- }
+  let shades = [
+    ...lightScale.slice(1).reverse(),
+    config.baseValue,
+    ...darkScale.slice(1)
+  ];
+
+  let names = [
+    ...lightScale.slice(1).map((_, i) => `l${i + 1}`).reverse(),
+    "base",
+    ...darkScale.slice(1).map((_, i) => `d${i + 1}`)
+  ];
+
+  let fixedHues = shades.map((color) => chroma(color).get("hsl.h").toFixed(2));
+
+  // Apply manual adjustments
+  if (config.manualAdjustments) {
+    const { lightness, saturation } = config.manualAdjustments;
+    shades = shades.map((color, index) => {
+      let adjustedColor = chroma(color).set("hsl.h", fixedHues[index]);
+      if (lightness && lightness[index] !== undefined) {
+        adjustedColor = adjustedColor.set("hsl.l", lightness[index]);
+      }
+      if (saturation && saturation[index] !== undefined) {
+        adjustedColor = adjustedColor.set("hsl.s", saturation[index]);
+      }
+      return adjustedColor.hex();
+    });
+  }
+
+  let output = {};
+  shades.forEach((color, index) => {
+    output[names[index]] = {
+      value: color,
+      type: "color",
+    };
+  });
+
+  return output;
+}
+
